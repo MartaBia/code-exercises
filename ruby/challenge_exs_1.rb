3014790d0f3ef9a73b39bd3f7ff4913ac987058d
Marta Bianchini <marta.bianchini24@gmail.com>
2022-07-25 23:43:53 +0100

    challenge exs - commented how I solved the exs later and left the version I did in the real challenge

--- ruby/challenge_exs_1.rb
+++ ruby/challenge_exs_1.rb
@@ -7,39 +7,39 @@ def change_letter(str)
 
 # -- How I resolved it in the test -----------------
 
-#   characters.each do |character|
-#     i = alphabet.find_index(character)
-#     if i == nil
-#       new_str += character
-#     else
-#       new_letter = alphabet[(i + 1) % alphabet.length]
-#       if vowels.include?(new_letter)
-#         new_str += new_letter.upcase
-#       else
-#         new_str += new_letter
-#       end
-#     end
-#   end
-#   new_str
-# end
+  characters.each do |character|
+    i = alphabet.find_index(character)
+    if i == nil
+      new_str += character
+    else
+      new_letter = alphabet[(i + 1) % alphabet.length]
+      if vowels.include?(new_letter)
+        new_str += new_letter.upcase
+      else
+        new_str += new_letter
+      end
+    end
+  end
+  new_str
+end
 
 # -- How I resolved it later ----------------------
 
-characters.each do |character|
-  if alphabet.include?(character)
-    i = alphabet.find_index(character)
-    new_letter = alphabet[(i + 1) % alphabet.length]
-    if vowels.include?(new_letter)
-      new_str += new_letter.upcase
-    else
-      new_str += new_letter
-    end
-  else
-    new_str += character
-  end
-end
-new_str
-end
+# characters.each do |character|
+#   if alphabet.include?(character)
+#     i = alphabet.find_index(character)
+#     new_letter = alphabet[(i + 1) % alphabet.length]
+#     if vowels.include?(new_letter)
+#       new_str += new_letter.upcase
+#     else
+#       new_str += new_letter
+#     end
+#   else
+#     new_str += character
+#   end
+# end
+# new_str
+# end
 
 
 
