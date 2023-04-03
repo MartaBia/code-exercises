b23c5e669b3f82045d7051313aecedef1b605085
Marta Bianchini <marta.bianchini24@gmail.com>
2023-04-03 14:56:27 +0100

    refactored

--- ruby/code-review-exs/exs3/lib/example.rb
+++ ruby/code-review-exs/exs3/lib/example.rb
@@ -1,3 +1,5 @@
 def is_spell_correct(sentence)
-  return sentence
+  sentence_array = sentence.split(' ')
+
+  return sentence_array.join(' ')
 end
--- ruby/code-review-exs/exs3/spec/example_spec.rb
+++ ruby/code-review-exs/exs3/spec/example_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'is_spell_correct' do
     expect(is_spell_correct('Hello')).to eq('Hello')
   end
 
-  it 'returns the given string with 2 corrected spelled words' do
+  it 'returns the given string with more than 1 word corrected spelled' do
     expect(is_spell_correct('Hello world')).to eq('Hello world')
   end
 end
