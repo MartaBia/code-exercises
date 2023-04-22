adc4ee034c576e17142f0f7179b120da7bd7e428
Marta Bianchini <marta.bianchini24@gmail.com>
2023-04-22 11:04:31 +0100

    factorial exs solved without math module

--- python/exercises/sum_factorial.py
+++ python/exercises/sum_factorial.py
@@ -1,14 +1,27 @@
 # In this kata you will be summing together multiple factorials.
 # you will be given a list of values that you must first find the factorial, and then return their sum.
 # For example if you are passed the list [4, 6] the equivalent mathematical expression would be 4! + 6! which would equal 744.
+
+# -- Cheating version importing math module --------------------------
 import math
 
 
+# def sum_factorial(lst):
+#     sum = 0
+#     for i in lst:
+#         factorial = math.factorial(i)
+#         sum += factorial
+#     return sum
+
+# -- Good girl version --------------------------
 def sum_factorial(lst):
     sum = 0
-    for i in lst:
-        factorial = math.factorial(i)
-        sum += factorial
+    for i in lst:  # i does not represent the index of the element but the element itself
+        fact = 1
+        for j in range(1, i+1):
+            fact *= j
+        sum += fact
+
     return sum
 
 
