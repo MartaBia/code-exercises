2ed6e79767f40cee669bb1ed8042075f433309a4
Marta Bianchini <marta.bianchini24@gmail.com>
2023-07-25 20:22:54 +0100

    set problem draw_the_stairs

---
+++ python/draw_the_syairs.py
@@ -0,0 +1,10 @@
+# Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+# For example n = 3 result in:
+
+# "I\n I\n  I"
+# or printed:
+
+# I
+#  I
+#   I
