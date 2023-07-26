d0b169ca045cc97149588ea343cd01a80dbdfd74
Marta Bianchini <marta.bianchini24@gmail.com>
2023-07-26 14:35:04 +0100

    set problem transform to prime

---
+++ python/transform_to_prime.py
@@ -0,0 +1,26 @@
+# Given a List [] of n integers , find minimum number to be inserted in a list,
+# so that sum of all elements of list should equal the closest prime number.
+
+# Notes
+# List size is at least 2.
+
+# List's numbers will only positives (n > 0).
+
+# Repetition of numbers in the list could occur.
+
+# The newer list's sum should equal the closest prime number.
+
+def minimum_number(numbers):
+    pass  # your code here
+
+
+print(minimum_number([3, 1, 2]))
+# => 1
+print(minimum_number([5, 2]))
+# => 0
+print(minimum_number([1, 1, 1]))
+# => 0
+print(minimum_number([2, 12, 8, 4, 6]))
+# => 5
+print(minimum_number([50, 39, 49, 6, 17, 28]))
+# => 2
