fadd16d26547e93d78de0655e5d2e59dfed6a14d
Marta Bianchini <marta.bianchini24@gmail.com>
2023-07-26 14:26:46 +0100

    Implemented draw_the_stairs

--- python/draw_the_syairs.py
+++ python/draw_the_syairs.py
@@ -8,3 +8,18 @@
 # I
 #  I
 #   I
+
+def draw_stairs(n):
+    final_string = ''
+    for i in range(n):
+        for j in range(i):
+            final_string += " "
+        final_string += "I\n"
+    return final_string[0:len(final_string)-1]
+
+
+print(draw_stairs(3))
+#  '''I\n I\n  I''')
+print("------------------")
+print(draw_stairs(5))
+# '''I\n I\n  I\n   I\n    I'''
