f34784685278bba36f922b435695b67e65a8d6b0
Marta Bianchini <marta.bianchini24@gmail.com>
2023-08-02 19:41:34 +0100

    third test pass

--- javascript/test/band_pass_filter.js
+++ javascript/test/band_pass_filter.js
@@ -2,6 +2,8 @@ const bandPassFilter = (soundWaves, lowestValue, highestValue) => {
   soundWave = soundWaves[0];
   if (soundWave < lowestValue) {
     return [lowestValue]
+  } else if (soundWave > highestValue) {
+    return [highestValue]
   } else {
     return soundWaves
   }
--- javascript/test/band_pass_filter.test.js
+++ javascript/test/band_pass_filter.test.js
@@ -8,4 +8,8 @@ describe("bandPassFilter", () => {
   it('returns a modified value when the soundwave is lower than the lowest value', () => {
     expect(bandPassFilter([30], 40, 70)).toEqual([40])
   })
+
+  it('returns a modified value when the soundwave is higher than the highest value', () => {
+    expect(bandPassFilter([1001], 40, 1000)).toEqual([1000])
+  })
 })
